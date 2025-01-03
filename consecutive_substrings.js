fc1065a946a15867b448872550c12812db08636d
David Doan <daviddoan@Davids-MacBook-Pro.local>
2025-01-03 00:09:58 -0700

    lab starter

---
+++ consecutive_substrings.js
@@ -0,0 +1,18 @@
+function consecutiveSubstrings(string) {
+  // type your code here
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
+  console.log("=>", consecutiveSubstrings('abc'));
+
+  console.log("");
+
+  console.log("Expecting: ['a']");
+  console.log("=>", consecutiveSubstrings('a'));
+}
+
+module.exports = consecutiveSubstrings;
+
+
