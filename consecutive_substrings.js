1d3386f24f1522c6fc3ad1e6a7cc2a8d8aef1f85
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-23 17:17:19 -0600

    added a solution for findinc consecutiveSubstrings

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,36 @@
 function consecutiveSubstrings(string) {
-  // type your code here
-}
+// type your code here
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+//empty array to store consecutive substring characters
+const consecutiveSubstrings = [];
+  //outer loop - sets anchor point
+  for (let i = 0; i < string.length; i++) {
+    //will hold current anchor character
+    let char = string[i];
+    //push current anchor character to consectutiveSubstrings array
+    consecutiveSubstrings.push(char);
+    for (let j = i + 1; j < string.length; j++) {
+      //will add consecutive next characters from anchor point through to end of array
+      char += string[j];
+      //push next consecutive characters to array
+      consecutiveSubstrings.push(char);
+    }
+  }
+  //returns the final consecutive combinations that were pushed through for iteration of each character
+  return consecutiveSubstrings;
+};
 
-  console.log("");
+console.log(consecutiveSubstrings(''));
 
-  console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
-}
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
+//   console.log("=>", consecutiveSubstrings('abc'));
 
-module.exports = consecutiveSubstrings;
+//   console.log("");
 
+//   console.log("Expecting: ['a']");
+//   console.log("=>", consecutiveSubstrings('a'));
+// }
 
+// module.exports = consecutiveSubstrings;
